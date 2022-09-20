26ddb8c4679ae016fde515565b0107a0256b8203
mohamedsabil83 <me@mohamedsabil83.com>
2022-09-20 23:11:27 +0300

    Clean code

--- src/FilamentHijriPickerServiceProvider.php
+++ src/FilamentHijriPickerServiceProvider.php
@@ -5,10 +5,8 @@ namespace Mohamedsabil83\FilamentHijriPicker;
 use Filament\Facades\Filament;
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FilamentHijriPickerServiceProvider extends PluginServiceProvider
-    // class FilamentHijriPickerServiceProvider extends PackageServiceProvider
 {
     protected array $beforeCoreScripts = [
         'filament-hijri-picker' => __DIR__.'/../resources/dist/js/hijri-date-time-picker.js',
@@ -29,9 +27,6 @@ class FilamentHijriPickerServiceProvider extends PluginServiceProvider
     public function packageBooted(): void
     {
         Filament::registerScripts([
-            // 'filament-hijri-picker' => __DIR__.'/../resources/dist/js/hijri-date-time-picker.js',
-            // 'filament-hijri-picker' => asset('vendor/filament-hijri-picker/tinymce/tinymce.min.js'),
-            // asset('vendor/filament-hijri-picker/js/filament-forms-range-component.min.css'),
         ]);
     }
 }
